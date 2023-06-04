17f1cabfa14300a611034631c8fc0fad77211bb9
Miguel Pinheiro <45045510+odbmeister@users.noreply.github.com>
2023-06-04 21:10:37 +0100

    Creation of a server to simulate the Wordpress API

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const cors = require('cors');
+const app = express();
+const port = 3000;
+
+app.use(cors());
+
+app.get('/data', (req, res) => {
+  const reviews = require('./data.json');
+  const filteredReviews = reviews.toplists['575'];
+
+  res.json(filteredReviews);
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
