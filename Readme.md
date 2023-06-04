7e751dba0402e24eb0633fce95b771d4ef0b3a64
Miguel Pinheiro <45045510+odbmeister@users.noreply.github.com>
2023-06-04 21:39:55 +0100

    Fix the data sorting + adding load more button working

--- Readme.md
+++ Readme.md
@@ -1,1 +1,13 @@
 # React App Fetching Data From API
+#
+#
+## To start the server with the WP API, go to the react_fetch_array_api folder, open a terminal and do this:
+node server.js
+#
+#
+## To start the Frontend, go to the react-fetch-api-app folder, open a terminal and do this:
+npm start
+#
+#
+#
+# Have fun checking the reviews :D
--- react-fetch-api-app/src/App.js
+++ react-fetch-api-app/src/App.js
@@ -2,16 +2,32 @@ import React, { useEffect, useState } from 'react';
 
 function App() {
   const [reviews, setReviews] = useState([]);
+  const [filteredData, setFilteredData] = useState([]);
+  const [initialReviewCount, setInitialReviewCount] = useState(3);
+
+  const loadMore = () => {
+    setInitialReviewCount(initialReviewCount + 3);
+  };
 
   useEffect(() => {
     fetchReviewsData();
   }, []);
 
+  useEffect(() => {
+    setFilteredData(reviews.slice(0, initialReviewCount));
+  }, [reviews, initialReviewCount]);
+
+  // fetching the data from the server/API and sorting it by the correct order
   const fetchReviewsData = async () => {
     try {
       const response = await fetch('http://localhost:3000/data');
       const data = await response.json();
-      setReviews(data);
+      const sortedData = data.sort((a, b) => {
+        if (a.brand_id < b.brand_id) return -1;
+        if (a.brand_id > b.brand_id) return 1;
+        return 0;
+      });
+      setReviews(sortedData);
     } catch (error) {
       console.log('Error fetching reviews:', error);
     }
@@ -20,8 +36,9 @@ function App() {
   return (
     <div className="App">
       <h1>Reviews</h1>
-      {reviews.map((review) => (
+      {filteredData.map((review) => (
         <div key={review.brand_id}>
+          <img src={review.logo} alt="Logo" />
           <h2>{review.brand_id}</h2>
           <p>Rating: {review.info.rating}</p>
           <ul>
@@ -30,10 +47,15 @@ function App() {
             ))}
           </ul>
           <p>Terms and Conditions: {review.terms_and_conditions}</p>
-          <img src={review.logo} />
           <a href={review.play_url}>PLAY NOW</a>
         </div>
       ))}
+      {reviews.length === 0 && <p>No reviews found.</p>}
+      {reviews.length > 0 && (
+        <button onClick={loadMore} disabled={initialReviewCount >= reviews.length}>
+        Load More
+      </button>
+      )}  
     </div>
   );
 }
